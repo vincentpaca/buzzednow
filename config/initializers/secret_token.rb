7782e3082f2aa12bb5d60f470560ebbafee370ba
Vincent Paca <vincent.paca@gmail.com>
2012-12-19 23:39:18 +0800

    Initial Rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buzzednow::Application.config.secret_token = 'c1980562b1e77d21d01094dfeeb7752bb3eeafb476ad2c0d2d915649f0d74ffb0d1ee6ecb2c81f6b417a5433c21f547fae8369532f8f511461714894d35ef863'
