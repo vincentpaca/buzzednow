1c46217c60e55c347002d3e1adcb2f224185e6a1
Vincent Paca <vincent.paca@gmail.com>
2012-12-25 18:57:41 +0800

    Added fonts path

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Buzzednow
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Fonts
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
