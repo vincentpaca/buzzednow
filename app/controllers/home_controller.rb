736358e81a3a747f89cf2c3563dd0e99b978838d
Vincent Paca <vincent.paca@gmail.com>
2012-12-27 01:51:20 +0800

    Added home page with popular and new arrivals

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,6 @@
 class HomeController < ApplicationController
+  def index
+    @new_arrivals = Item.new_arrivals
+    @popular_picks = Item.popular_picks
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,18 @@ class Item < ActiveRecord::Base
   attr_accessible :name, :price
 
   has_many :photos
+
+  def primary_photo
+    self.photos.first
+  end
+
+  class << self
+    def new_arrivals
+      Item.where("created_at > ?", 3.days.ago).limit(3)
+    end
+
+    def popular_picks
+      Item.where("created_at < ?", 3.days.ago).limit(8)
+    end
+  end
 end
