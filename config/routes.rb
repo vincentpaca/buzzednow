657c819592d117e1f08df02ba2b005c021b83fe5
Vincent Paca <vincent.paca@gmail.com>
2012-12-27 00:55:54 +0800

    Added sidekiq web interface

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Buzzednow::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
+  require 'sidekiq/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == "admin" && password == "passw0rd"
+  end
+  mount Sidekiq::Web => '/sidekiq'
+
   root :to => "home#index"
 end
