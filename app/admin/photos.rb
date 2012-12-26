9851f47fafc14ef2b244806161916223c6522379
Vincent Paca <vincent.paca@gmail.com>
2012-12-27 00:56:18 +0800

    Updated admin view for new photos

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Photo do
-  
+  form do |f|
+    f.inputs do
+      f.input :item
+      f.input :public, as: :file, input_html: { multiple: true }
+    end
+    f.buttons
+  end
+
+  controller do
+    def create
+      params[:photo][:public].each do |p|
+        Uploader.perform_async(p, params[:photo][:item_id])
+      end
+
+      flash[:notice] = "Processing images, please wait"
+      redirect_to admin_photos_url
+    end
+  end
 end
