2819b335aec0190d49b64060c0e642277eebb110
Vincent Paca <vincent.paca@gmail.com>
2012-12-27 01:05:29 +0800

    Added file upload without backgrounding

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -2,19 +2,19 @@ ActiveAdmin.register Photo do
   form do |f|
     f.inputs do
       f.input :item
-      f.input :public, as: :file, input_html: { multiple: true }
+      f.input :public, as: :file
     end
     f.buttons
   end
 
-  controller do
-    def create
-      params[:photo][:public].each do |p|
-        Uploader.perform_async(p, params[:photo][:item_id])
-      end
-
-      flash[:notice] = "Processing images, please wait"
-      redirect_to admin_photos_url
-    end
-  end
+  #controller do
+  #  def create
+  #    params[:photo][:public].each do |p|
+  #      Uploader.perform_async(p, params[:photo][:item_id])
+  #    end
+#
+ #     flash[:notice] = "Processing images, please wait"
+  #    redirect_to admin_photos_url
+  #  end
+  #end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ActiveRecord::Base
   attr_accessible :item_id, :public, :item
 
   belongs_to :item
+
+  mount_uploader :public, PhotoUploader
 end
