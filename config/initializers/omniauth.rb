9751737e2753814bbd1baac7123314c308cb2227
Vincent Paca <vincent.paca@gmail.com>
2012-12-29 22:03:04 +0800

    Use different facebook app for development

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
+
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '279796595475937', '8532c33961b7afbed15c5aeead5fdc8d'
+  if Rails.env.production?
+    provider :facebook, '279796595475937', '8532c33961b7afbed15c5aeead5fdc8d'
+  else
+    provider :facebook, '499756363401900', 'bf1ce2752ba13e7e7363a3ebf75bda5b'
+  end
 end
