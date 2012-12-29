66974a4f6c5a28c4af0bcf11221cfdce0e30d087
Vincent Paca <vincent.paca@gmail.com>
2012-12-29 22:37:53 +0800

    Added friendly redirect when logging in from reservation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
+    origin = request.env["omniauth.origin"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Sign in successful!"
+
+    if origin
+      redirect_to "#{origin}"
+    else
+      redirect_to root_url, :notice => "Sign in successful!"
+    end
   end
 
   def destroy
