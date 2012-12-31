e7d44f6db1de736d26894f3bafbd81f8f12af0bf
Vincent Paca <vincent.paca@gmail.com>
2012-12-31 21:12:51 +0800

    Added search and filter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,18 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.includes(:photos)
+
+    if params[:sub_category]
+      @items = @items.where("sub_category_id = ?", params[:sub_category])
+    end
+
+    if params[:q]
+      @items = @items.where("name ilike ?", "%#{params[:q]}%")
+    end
   end
 
   def show
-    @item = Item.find(params[:id]).includes(:photos)
+    @item = Item.includes(:photos).find(params[:id])
     @images = @item.photos
   end
 end
