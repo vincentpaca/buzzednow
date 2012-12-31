9c20273e5760794eddeaef3c8fadc61507bf6266
Vincent Paca <vincent.paca@gmail.com>
2012-12-31 21:05:21 +0800

    Fixed filter method conflicting with activeadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_categories
 
-  helper_method :current_user
+  helper_method :current_user, :categories
 
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def set_categories
-    @categories = Category.includes(:sub_categories)
+  def categories
+    Category.includes(:sub_categories)
   end
 end
